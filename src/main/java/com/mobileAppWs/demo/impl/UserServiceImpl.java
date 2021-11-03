9410fa7fb84db2c59a05c9dd2a0db386b7f33428
Hyesung Park <jayhspark@gmail.com>
2021-11-03 16:27:50 +0900

    update

--- src/main/java/com/mobileAppWs/demo/impl/UserServiceImpl.java
+++ src/main/java/com/mobileAppWs/demo/impl/UserServiceImpl.java
@@ -7,6 +7,8 @@ import com.mobileAppWs.demo.repository.UserRepository;
 import com.mobileAppWs.demo.services.UserService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -41,5 +43,10 @@ public class UserServiceImpl implements UserService {
     BeanUtils.copyProperties(storedUserDetails, returnValue);
 
     return returnValue;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+    return null;
   }
 }
