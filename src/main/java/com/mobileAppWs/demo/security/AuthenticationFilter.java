e20f46c203968b10f71798cb2b16e007f6a09fcd
Hyesung Park <jayhspark@gmail.com>
2021-11-04 11:57:58 +0900

    update

--- src/main/java/com/mobileAppWs/demo/security/AuthenticationFilter.java
+++ src/main/java/com/mobileAppWs/demo/security/AuthenticationFilter.java
@@ -1,7 +1,10 @@
 package com.mobileAppWs.demo.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mobileAppWs.demo.SpringApplicationContext;
+import com.mobileAppWs.demo.common.dto.UserDto;
 import com.mobileAppWs.demo.models.request.UserLoginRequestModel;
+import com.mobileAppWs.demo.services.UserService;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -53,7 +56,11 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
             .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
             .signWith(SignatureAlgorithm.HS512, SecurityConstants.TOKEN_SECRET)
             .compact();
+    UserService userService = (UserService) SpringApplicationContext.getBean("userServiceImpl");
+    UserDto userDto = userService.getUser(userName);
+
 
     res.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
+    res.addHeader("UserID", userDto.getUserId());
   }
 }
--- src/main/java/com/mobileAppWs/demo/security/WebSecurity.java
+++ src/main/java/com/mobileAppWs/demo/security/WebSecurity.java
@@ -14,7 +14,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
   private final UserService userDetailsService;
   private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-  public WebSecurity(UserService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder){
+  public WebSecurity(UserService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
     this.userDetailsService = userDetailsService;
     this.bCryptPasswordEncoder = bCryptPasswordEncoder;
   }
@@ -22,7 +22,15 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
 
-    http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll().anyRequest().authenticated().and().addFilter(new AuthenticationFilter(authenticationManager()));
+    http.csrf()
+        .disable()
+        .authorizeRequests()
+        .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .addFilter(getAuthenticationFilter());
   }
 
   @Override
@@ -30,4 +38,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
   }
 
+  public AuthenticationFilter getAuthenticationFilter() throws Exception {
+    final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+    filter.setFilterProcessesUrl("/users/login");
+    return filter;
+  }
 }
