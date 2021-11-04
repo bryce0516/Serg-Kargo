20ddd499efab4d9c6ba63a106dc8bf4b2a2bbd33
Hyesung Park <jayhspark@gmail.com>
2021-11-04 13:01:39 +0900

    update

--- src/main/java/com/mobileAppWs/demo/MobileAppWsApplication.java
+++ src/main/java/com/mobileAppWs/demo/MobileAppWsApplication.java
@@ -1,5 +1,6 @@
 package com.mobileAppWs.demo;
 
+import com.mobileAppWs.demo.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -22,4 +23,8 @@ public class MobileAppWsApplication {
 	  return new SpringApplicationContext();
   }
 
+  @Bean(name="AppProperties")
+  public AppProperties getAppProperties(){
+	  return new AppProperties();
+  }
 }
