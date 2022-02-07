9088b6d1547042bb8cdec67e7cae61d0050c04b1
Hyesung Park <jayhspark@gmail.com>
2022-02-07 19:55:24 +0900

    update

---
+++ src/main/java/com/mobileAppWs/demo/test/model/CommonResult.java
@@ -0,0 +1,34 @@
+package com.mobileAppWs.demo.test.model;
+
+public class CommonResult {
+
+  private boolean success;
+  private int code;
+  private String msg;
+
+  public boolean isSuccess() {
+    return success;
+  }
+
+  public void setSuccess(boolean success) {
+    this.success = success;
+  }
+
+  public int getCode() {
+    return code;
+  }
+
+  public void setCode(int code) {
+    this.code = code;
+  }
+
+  public String getMsg() {
+    return msg;
+  }
+
+  public void setMsg(String msg) {
+    this.msg = msg;
+  }
+
+
+}
