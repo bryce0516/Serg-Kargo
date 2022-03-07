cf517fd76cd721ed52e5526be8e191bcbe26cb63
bryce0516 <jayhspark@gmail.com>
2022-03-07 20:48:59 +0900

    update

--- src/main/java/com/mobileAppWs/demo/services/FileService.java
+++ src/main/java/com/mobileAppWs/demo/services/FileService.java
@@ -2,7 +2,12 @@ package com.mobileAppWs.demo.services;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Service
 public class FileService {
@@ -11,7 +16,17 @@ public class FileService {
     private String filesPath;
 
     public Resource download(String filename) {
-
+        try{
+            Path file = Paths.get(filesPath).resolve(filename);
+            Resource resource = new UrlResource(file.toUri());
+            if(resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new RuntimeException("Could not read the file!");
+            }
+        }catch (MalformedURLException e) {
+            throw new RuntimeException("Error: " + e.getMessage());
+        }
     }
 
 }
